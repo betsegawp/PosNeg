ad0905235c06b0aa20064e1338c081cc92dfbd82
betsegawp <betsegaw.tereda@gmail.com>
2020-08-25 14:55:55 -0400

    pos

---
+++ src/positive.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class positive {
+
+    public static void main(String[] args) {
+Scanner input= new Scanner(System.in);
+int testData = 0;
+        System.out.println("Enter your test Data: ");
+         testData =input.nextInt();
+        {
+            if(testData >= 0)
+            {
+                System.out.println ( testData+ " is Positive");
+                        }
+              else
+                    {
+                    System.out.println( testData+ " is Negative");
+
+                }
+            }
+        }
+    }
+
+
